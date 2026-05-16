0595e73e147a8a18b7894164c676d20b4bef52a2
Himali Paneru <himalipaneru024@gmail.com>
2026-05-16 23:29:15 +0530

    fix uploads static path

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const societyRoutes = require("./routes/societyRoutes");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use("/api/societies", societyRoutes);
 
 app.get("/", (req, res) => {
